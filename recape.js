408e6cced19ec80a27666b51aa7b6939e5a809b0
Your Name <minajmiha02@gmail.com>
2026-02-05 20:35:26 +0600

    recape

---
+++ recape.js
@@ -0,0 +1,15 @@
+const tex = 5000;
+let eta = 5;
+eta = 3;
+const student = {
+  name: "arifa",
+  markes: 50,
+};
+const friend = ["abul", "babul", "cabul"];
+function add(num1, num2) {
+  console.log(num1 + num2);
+}
+const dynamicTaxt = ` i need ${num2}`;
+const interHTML = `<div><h1>Hello: ${friend?.length}</h1><p></p></div>`;
+const tenTimes = (x) => x * 10;
+const newFriend = [...friend,"arifa","yeasmin"]
