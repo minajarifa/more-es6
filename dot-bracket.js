da72556d570586cbf5fab2a6087a4767e9cd3074
Your Name <minajmiha02@gmail.com>
2026-02-06 18:49:50 +0600

    added dot-ent

--- dot-bracket.js
+++ dot-bracket.js
@@ -2,8 +2,8 @@ const student = {
   name: "nur uddin kamran",
   markes: 40,
   1: 50,
-  "home-address": "kocukhet",
+  "home-address": "kocu khet",
 };
 const studentName = student.name;
 const studentName2 = student["home-address"];
-console.log(student);
+console.log(student["home-address"]);
