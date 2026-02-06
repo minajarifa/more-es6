e406d563cc89fff6d5584f89a55097e6d2fbf4e1
Your Name <minajmiha02@gmail.com>
2026-02-06 16:45:42 +0600

    start 2nd

--- dot-bracket.js
+++ dot-bracket.js
@@ -1,6 +1,9 @@
 const student ={
     name:"nur uddin kamran",
     markes: 40,
+    1:50
 }
-const studentName= student.name;
+// const studentName= student.name;
+const studentName= student[1];
+
 console.log(studentName);
